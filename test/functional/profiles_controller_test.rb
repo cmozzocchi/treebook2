7aa8c4556f85a7f3a132ab3d0951c1230c175dbd
Chris Mozzocchi <cmozzocchi@gmail.com>
2012-11-08 11:50:37 -0600

    added gravatar functionality

---
+++ test/functional/profiles_controller_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class ProfilesControllerTest < ActionController::TestCase
+  
+  test "should get show" do
+    get :show, id: users(:chris).profile_name
+    assert_response :success
+    assert_template 'profiles/show'
+  end
+
+  test 'should render 404 if profile is not found' do
+  	get :show, id: "doesn't exist"
+  	assert_response :not_found
+  end
+
+  test 'that variables are assinged on successful profile viewing' do
+  	get :show, id: users(:chris).profile_name
+  	assert assigns(:user)
+  	assert_not_empty assigns(:statuses)
+  end
+
+  test 'only show the correct user statuses' do
+  	get :show, id: users(:chris).profile_name
+  	assigns(:statuses).each do |status|
+  		assert_equal users(:chris), status.user 
+  	end
+  end
+
+
+
+end
