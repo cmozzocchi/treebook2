f796269da99eaed75f42ce04c96fc25028c8e079
Chris Mozzocchi <cmozzocchi@gmail.com>
2013-02-04 12:40:01 -0600

    updated gems

--- test/functional/user_friendships_controller_test.rb
+++ test/functional/user_friendships_controller_test.rb
@@ -14,6 +14,7 @@ class UserFriendshipsControllerTest < ActionController::TestCase
       setup do
         sign_in users(:chris)
       end
+      
       should "get new and return 200" do
         get :new
         assert_response :success
